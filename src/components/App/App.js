8f9914472846c8e4f96cf2302efc3a203f92c96b
Myron <dubtwee@gmail.com>
2020-04-10 10:44:58 -0500

    showing all songs listed on the page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,11 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
+  state = {
+    songsList: []
+  }
 
+  // this method runs when component is mounted to the the App / page
   componentDidMount() {
     console.log('Component App Did Mount');
     this.getSongs();
@@ -19,14 +23,37 @@ class App extends Component {
     })
     .then((response) => {
       // update state with server data
-      console.log(response.data);
+      console.log('Server Response:', response.data);
+      // store songs in local state
+      this.setState(
+        {
+          songsList: [
+            ...response.data
+          ]
+        },
+        () => {
+          console.log('NEW SONGS?', this.state.songsList);
+        }
+      );
     })
     .catch((err) => {
       // surface error message to user
+      alert('There was an issue getting songs. Please try again later.');
     });
   }
 
   render() {
+    const displayedSongs = this.state.songsList.map((song, index) => {
+      return (
+        <div key={index}>
+          <h3>{song.track}</h3>
+          <em>- by, {song.artist}</em>
+          <p>Rank: {song.rank}</p>
+          <p>{song.published}</p>
+        </div>
+      )
+    });
+
     return (
       <div className="App">
         <header className="App-header">
@@ -34,7 +61,10 @@ class App extends Component {
         </header>
 
         <br />
-        <p>Songs go here</p>
+        <h2>Songs List</h2>
+        <div>
+          {displayedSongs}
+        </div>
       </div>
     );
   }
