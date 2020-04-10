13a8422d221039de0047e3e42edd58734aefbd6d
Myron <dubtwee@gmail.com>
2020-04-10 10:30:48 -0500

    setting up server data retrieval on component loading

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  componentDidMount() {
+    console.log('Component App Did Mount');
+    this.getSongs();
+  }
+
+  getSongs() {
+    // $.ajax(
+    //   {}
+    // )
+    axios({
+      method: 'GET',
+      url: '/songs'
+    })
+    .then((response) => {
+      // update state with server data
+      console.log(response.data);
+    })
+    .catch((err) => {
+      // surface error message to user
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Songs!</h1>
         </header>
-        <br/>
+
+        <br />
         <p>Songs go here</p>
       </div>
     );
